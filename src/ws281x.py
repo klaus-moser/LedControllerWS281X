87ea141553ac3f9b095246f4110bd5525fc9a950
klaus-moser <60796711+klaus-moser@users.noreply.github.com>
2024-04-16 19:33:16 +0200

    Update name + Readme

--- src/ws281x.py
+++ src/ws281x.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
 
-# Filename: led_controller.py
+# Filename: ws281x.py
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+RPI WS281X PixelStrip driver.
+https://github.com/klaus-moser/LedControllerWS281X
 
+MIT License
+Copyright (c) 2024 Klaus Moser
 
-""" LedController Class to create an information-bit-string for the 'rpi_ws281x PixelStrip' """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 
 import os
 import time
 
-__version__ = "0.1"
+__version__ = "0.0.1"
 __author__ = "klaus-moser"
 __date__ = time.ctime(os.path.getmtime(__file__))
 
 
-class LedController:
+class LedControllerWS281X:
     """
-    LedController Class.
+    LedControllerWS281X Class.
 
     Example:
 
@@ -275,5 +290,5 @@ class LedController:
 
 if __name__ == "__main__":
     # Example
-    e = LedController(cols=49, rows=8)
+    e = LedControllerWS281X(cols=49, rows=8)
     e.run(text="0 1 2 3 4 5 6 7 8 9", show=True)
